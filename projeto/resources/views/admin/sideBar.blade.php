f2a1d946860b9cf397c03ec21e39bbc939cbdaee
eduardoeiras <61152075+eduardoeiras@users.noreply.github.com>
2020-12-06 20:48:12 +0000

    projeto3

---
+++ projeto/resources/views/admin/sideBar.blade.php
@@ -0,0 +1,12 @@
+<div class="bg-light border-right" id="sidebar-wrapper" style="height=100%">
+      <div class="sidebar-heading">Opções de Gestão</div>
+      <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gerir Utilizadores</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gerir Projetos</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gerir Ilustradores Solidários</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gerir Contadores de Histórias</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gerir Universidades</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Gerir Professores de Faculdades</a>
+      </div>
+</div>
+
