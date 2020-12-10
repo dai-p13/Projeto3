21d04b03ba140362e9a9255cffc9512d34d377bd
dai-p13 <ds2000117@gmail.com>
2020-12-10 18:19:43 +0000

    UniversidadeController e js

--- projeto/app/Http/Controllers/UniversidadeController.php
+++ projeto/app/Http/Controllers/UniversidadeController.php
@@ -4,93 +4,115 @@ namespace App\Http\Controllers;
 
 use App\Models\Universidade;
 use Illuminate\Http\Request;
+use DB;
 
 class UniversidadeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
+        $uni = session()->get("utilizador");
         $universidades = Universidade::all();
+        if($uni->tipoUtilizador == 0) {
+            return view('admin/universidade', ['universidades' => $universidades]);
+        }
+        else {
+            return view('colaborador/universidade', ['universidades' => $universidades]);
+        }
 
-        return view('viewUniversidade', ['universidades' => $universidades]);
+        //return view('viewUniversidade', ['universidades' => $universidades]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $universidade = new Universidade();
-
-        $universidade->curso = $request->curso;
-        $universidade->tipo = $request->tipo;
-        $universidade->nome = $request->nome;
-        $universidade->telefone = $request->telefone;
-        $universidade->telemovel = $request->telemovel;
-        $universidade->email = $request->email;
-
-        $universidade->save();
+        $curso = $request->get("curso");
+        $tipo = $request->get("tipo");
+        $nome = $request->get("nome");
+        $telefone = $request->get("telefone");
+        $telemovel = $request->get("telemovel");
+        $email = $request->get("email");
+        
+        $universidade = self::getUniNome($nome);
+        if($universidade != null) {
+            $universidade = new Universidade();
+            
+            $universidade->curso = $request->curso;
+            $universidade->tipo = $request->tipo;
+            $universidade->nome = $request->nome;
+            $universidade->telefone = $request->telefone;
+            $universidade->telemovel = $request->telemovel;
+            $universidade->email = $request->email;
+            
+            $universidade->save();
+            return redirect()->route("universidade");
+        }
+        
+    }
+    
+    public function update($id, Request $request)
+    {
+        $id_universidade = \intval($id);
+        $curso = $request->get("curso");
+        $tipo = $request->get("tipo");
+        $nome = $request->get("nome");
+        $telefone = $request->get("telefone");
+        $telemovel = $request->get("telemovel");
+        $email = $request->get("email");
+        
+        $universidade = Universidade::find($id_universidade);
+        if($universidade != null) {            
+            $universidade->curso = $curso;
+            $universidade->tipo = $tipo;
+            $universidade->nome = $nome;
+            $universidade->telefone = $telefone;
+            $universidade->telemovel = $telemovel;
+            $universidade->email = $email;
+            
+            $universidade->save();
+            return redirect()->route("universidade");
+        }
+    }
+    
+    public function destroy($id)
+    {
+       $universidade = Universidade::find($id);
+       $universidade->delete();
+       return redirect()->route("universidade"); 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Universidade  $universidade
-     * @return \Illuminate\Http\Response
-     */
-    public function show(universidade $universidade)
+    public function getUniId($id)
     {
-        //
+        return ['universidade' => Universidade::findOrFail($id)];
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Universidade  $universidade
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(universidade $universidade)
+    public function getUserNome($nome)
     {
-        //
+        $nomeUni = DB::table('universidade')->where('nome', $nome)->first();
+        return $nomeUni;
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Universidade  $universidade
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, universidade $universidade)
-    {
-        //
+    public function getUniPorId($id) {
+        
+        $uni = DB::table('universidade')->where('id_uuniversidade', $id)->first();
+        if($uni != null) {
+            return response()->json($uni);  
+        }
+        else {
+            return null;
+        }
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Universidade  $universidade
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(universidade $universidade)
-    {
-        //
+    public function getNumUni() {
+
+        $universidades = Universidade::all();
+        
+        if($universidades != null) {
+            return \count($universidades);
+        }
+        else {
+            return 0;
+        }
+        
     }
 }
