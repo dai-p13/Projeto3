f837b5a0b540c0744055197b6283a1d7560095ba
dai-p13 <ds2000117@gmail.com>
2020-12-28 15:05:57 +0000

    entidadesOficiaisfaltatestefinal

--- projeto/app/Http/Controllers/EntidadeOficialController.php
+++ projeto/app/Http/Controllers/EntidadeOficialController.php
@@ -5,94 +5,80 @@ namespace App\Http\Controllers;
 use App\Models\EntidadeOficial;
 use Illuminate\Http\Request;
 use DB;
-
+use Session;
 class EntidadeOficialController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
-        $entOficiais = EntidadeOficial::all();
-
-        return view('viewEntidadeOficial', ['entOficiais' => $entOficiais]);
+        $user = session()->get("utilizador");
+        $entidades = EntidadeOficial::all();
+        if($user->tipoUtilizador == 0) {
+            return view('admin/entidades', ['data' => $entidades]);
+        }
+        else {
+            return view('colaborador/entidades', ['data' => $entidades]);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $entoficial = new EntidadeOficial();
+        $entOficial = new EntidadeOficial();
 
-        $entoficial->nome = $request->nome;
-        $entoficial->email = $request->email;
-        $entoficial->entidade = $request->entidade;
-        $entoficial->telefone = $request->telefone;
-        $entoficial->telemove = $request->telemovel;
-        $entoficial->observacoes = $request->observacoes;
+        $entOficial->nome = $request->nome;
+        $entOficial->email = $request->email;
+        $entOficial->entidade = $request->entidade;
+        $entOficial->telefone = $request->telefone;
+        $entOficial->telemovel = $request->telemovel;
+        $entOficial->observacoes = $request->observacoes;
 
-        $entoficial->save();
+        $entOficial->save();
+        return redirect()->route("entidades");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\EntidadeOficial  $entoficial
-     * @return \Illuminate\Http\Response
-     */
-    public function show(entoficial $entoficial)
+    public function update($id ,Request $request)
     {
-        //
+        $id_entidadeOficial = \intval($id);
+        $nome = $request->nome;
+        $email = $request->email;
+        $entidade = $request->entidade;
+        $telefone = $request->telefone;
+        $telemovel = $request->telemovel;
+        $observacoes = $request->obs;
+
+        $entOficial = EntidadeOficial::find($id_entidadeOficial);
+        if($entOficial != null){
+            $entOficial->nome = $nome;
+            $entOficial->email = $email;
+            $entOficial->entidade = $entidade;
+            $entOficial->telefone = $telefone;
+            $entOficial->telemovel = $telemovel;
+            $entOficial->observacoes = $observacoes;
+
+            $entOficial->save();
+            return redirect()->route("entidades");
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\EntidadeOficial  $entoficial
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(entoficial $entoficial)
+    public function destroy($id)
     {
-        //
+        $entidades = EntidadeOficial::find($id);
+        if($entidades->projetos()->first() != null) {
+            $entidades->projetos()->where('id_entidadeOficial', $id)->delete();
+        }
+        $entidades->delete();
+        return redirect()->route("entidades");
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\EntidadeOficial  $entoficial
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, entoficial $entoficial)
+    public function getEntidadePorId($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\EntidadeOficial  $entoficial
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(entoficial $entoficial)
-    {
-        //
+        $entidades = DB::table('entidade_oficial')->where('id_entidadeOficial', $id)->first();
+        if($entidades != null) {
+            return response()->json($ilustrador);
+        }
+        else{
+            return null;
+        }
     }
 
     public function getDisponiveis() {
